e107a303029fcbd198d9a812331becc84b2f60fc
GeetikaSejwal <geetikasejwalmon@gmail.com>
2021-04-12 01:05:02 +0530

    commit on master

---
+++ app/Http/Controllers/productController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\products;
+class productController extends Controller
+{
+    function index(){
+        return "product";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\productController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('login', function () {
     return view('login');
 });
+Route::get('/',[productController::class,'index']);
